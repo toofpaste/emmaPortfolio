9a366502670b8835aaa291cc66af7bdc3b71eaa6
Michael Larragueta <mjlarragueta@gmail.com>
2020-02-27 12:08:40 -0800

    Add screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './styles.css';
-import gif from './img/test.gif';
+import gif from './img/portfolio-eblm1.gif';
+import gif1 from './img/portfolio-eblm2.gif';
+import gif2 from './img/portfolio-expensive.gif';
+import gif3 from './img/portfolio-letugo1.gif';
+// import gif4 from './img/portfolio-mad1.gif';
+import gif5 from './img/portfolio-mad2.gif';
+import gif6 from './img/portfolio_gyw.gif';
+import gif7 from './img/portfolio_language1.gif';
+import gif8 from './img/portfolio_language2.gif';
+import gif9 from './img/portfolio_mad3.gif';
+import gif10 from './img/portfolio_mad4.gif';
+import gif11 from './img/portfolio_pickitup1.gif';
+import gif12 from './img/portfolio_pickitup2.gif';
+import gif13 from './img/portfolio_shake1.gif';
+import gif14 from './img/portfolio_shake2.gif';
+import gif15 from './img/portfolio_shake3.gif';
+// import gif16 from './img/portfolio_shake4.gif';
 const ele = document.getElementById("test");
 const ele1 = document.getElementById("test1");
 const ele2 = document.getElementById("test2");
@@ -29,23 +45,27 @@ const ele25 = document.getElementById("test25");
 import pd from './ResumeSeptember2019.pdf';
 const elep = document.getElementById('resume');
 elep.href = pd;
-ele.src = gif;
-ele1.src = gif;
-ele2.src = gif;
-ele3.src = gif;
-ele4.src = gif;
-ele5.src = gif;
+ele.src = gif5;
+ele1.src = gif2;
+ele2.src = gif10;
+
+ele3.src = gif7;
+ele4.src = gif8;
+ele5.src = gif9;
+
 ele6.src = gif;
-ele7.src = gif;
-ele8.src = gif;
-ele9.src = gif;
-ele10.src = gif;
-ele11.src = gif;
-ele12.src = gif;
-ele13.src = gif;
-ele14.src = gif;
-ele15.src = gif;
-ele16.src = gif;
+ele7.src = gif3;
+ele8.src = gif1;
+
+ele9.src = gif11;
+ele10.src = gif6;
+ele11.src = gif12;
+
+ele12.src = gif13;
+ele13.src = gif15;
+ele14.src = gif14;
+// ele15.src = gif16;
+// ele16.src = gif16;
 ele17.src = gif;
 ele18.src = gif;
 ele19.src = gif;
@@ -57,43 +77,43 @@ ele24.src = gif;
 ele25.src = gif;
 $(document).ready(function(){
 });
-
-$(window).on("scroll", function() {
-    var scrollTop = $(window).scrollTop();
-    var oddImgPos = scrollTop / 3 + 'px';
-    var evenImgPos = (scrollTop/5) / 2.5 + 'px';
-    var p = $( "#p2" ).first();
-    var position = p.position();
-    //all end at 255
-    // $( "#p1" ).first().position().top;
-    // $( "#p2" ).first().position().top;
-    // $( "#p3" ).first().position().top;
-    // $( "#p4" ).first().position().top;
-    // $( "#p5" ).first().position().top;
-    // $( "#p6" ).first().position().top;
-    // $( "#p7" ).first().position().top;
-    // $( "#p8" ).first().position().top;
-    // $( "#p9" ).first().position().top;
-    // $( "#p10" ).first().position().top;
-    // $( "#p11" ).first().position().top;
-    // $( "#p12" ).first().position().top;
-    // $( "#p13" ).first().position().top;
-    // $( "#p14" ).first().position().top;
-    // $( "#p15" ).first().position().top;
-    // $( "#p16" ).first().position().top;
-    // $( "#p17" ).first().position().top;
-    // $( "#p18" ).first().position().top;
-    // $( "#p19" ).first().position().top;
-    // $( "#p20" ).first().position().top;
-    // $( "#p21" ).first().position().top;
-    // $( "#p22" ).first().position().top;
-    // $( "#p23" ).first().position().top;
-    // $( "#p24" ).first().position().top;
-    // $( "#p25" ).first().position().top;
-    // $( "#p26" ).first().position().top;
-            $('.grid').find("p").css('transform', 'translateY(-' + evenImgPos + ')');
-        // $('#img2').find("#p2").css('transform', 'translateY(-' + evenImgPos + ')');
-    // if($('#img2').is(":visible")) {
-    //     $('#img2').find("#p2").css('transform', 'translateY(-' + evenImgPos + ')');
-    // }
-});
+//
+// $(window).on("scroll", function() {
+//     var scrollTop = $(window).scrollTop();
+//     var oddImgPos = scrollTop / 3 + 'px';
+//     var evenImgPos = (scrollTop/5) / 2.5 + 'px';
+//     var p = $( "#p2" ).first();
+//     var position = p.position();
+//     //all end at 255
+//     // $( "#p1" ).first().position().top;
+//     // $( "#p2" ).first().position().top;
+//     // $( "#p3" ).first().position().top;
+//     // $( "#p4" ).first().position().top;
+//     // $( "#p5" ).first().position().top;
+//     // $( "#p6" ).first().position().top;
+//     // $( "#p7" ).first().position().top;
+//     // $( "#p8" ).first().position().top;
+//     // $( "#p9" ).first().position().top;
+//     // $( "#p10" ).first().position().top;
+//     // $( "#p11" ).first().position().top;
+//     // $( "#p12" ).first().position().top;
+//     // $( "#p13" ).first().position().top;
+//     // $( "#p14" ).first().position().top;
+//     // $( "#p15" ).first().position().top;
+//     // $( "#p16" ).first().position().top;
+//     // $( "#p17" ).first().position().top;
+//     // $( "#p18" ).first().position().top;
+//     // $( "#p19" ).first().position().top;
+//     // $( "#p20" ).first().position().top;
+//     // $( "#p21" ).first().position().top;
+//     // $( "#p22" ).first().position().top;
+//     // $( "#p23" ).first().position().top;
+//     // $( "#p24" ).first().position().top;
+//     // $( "#p25" ).first().position().top;
+//     // $( "#p26" ).first().position().top;
+//             $('.grid').find("p").css('transform', 'translateY(-' + evenImgPos + ')');
+//         // $('#img2').find("#p2").css('transform', 'translateY(-' + evenImgPos + ')');
+//     // if($('#img2').is(":visible")) {
+//     //     $('#img2').find("#p2").css('transform', 'translateY(-' + evenImgPos + ')');
+//     // }
+// });
